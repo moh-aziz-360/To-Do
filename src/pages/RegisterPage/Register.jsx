f7c307d65a4e765c0799f16a68d6c5c4ac8c1263
Sophont-360 <abdulaziznexxl@gmai.com>
2024-08-07 16:21:45 +0000

    Update firebase configuration and handle auth error

--- src/pages/RegisterPage/Register.jsx
+++ src/pages/RegisterPage/Register.jsx
@@ -1,4 +1,3 @@
-// src/pages/Register.jsx
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
